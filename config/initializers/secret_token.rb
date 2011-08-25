dd3e7e50e0df7066e920361ae5098f4ff577f604
Roger Braun <roger@rogerbraun.net>
2011-08-25 18:55:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zui::Application.config.secret_token = '717ac53580369333e1b34b62333787d32df0a905736b4b99c87907a32721be74213c2b25f457ebe14f951f1cc76771e3e9db4b788cee3e8ed6a63cfaa8ce8eaf'
