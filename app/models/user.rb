4b7e91763ac50e98e14a52d136b7c2f9fb477b69
Roger Braun <roger@rogerbraun.net>
2011-08-31 17:04:47 +0200

    added groups to user, make devise views explicit, add roadmap

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def admin?
+    group.split(",").include?("admin")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+# Make admin user
+#
+admin = User.new
+admin.email = "admin@localhost.com"
+admin.password = "adminadmin"
+admin.group = "admin"
+admin.save
