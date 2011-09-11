ff8fc5ab809e43f8e34c8f36d3b9441adb4b9b20
Roger Braun <davinelulinvega@gmail.com>
2011-09-12 01:24:15 +0200

    add project and activity view to start page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @projects = current_user.projects + current_user.shared_projects
+      @comments = Comment.limit(10).order("updated_at desc").all
+    end
   end
 
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def activity_helper(comments)
+    
+  end
 end
