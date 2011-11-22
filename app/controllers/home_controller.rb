392ae60e9e775fc2d1128c5357929764d89f723f
Roger Braun <davinelulinvega@gmail.com>
2011-11-23 00:05:25 +0100

    some small fixes (blind texts, activity display, fragment form)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @projects = current_user.projects + current_user.shared_projects
-      @comments = Comment.limit(10).order("updated_at desc").all
+      @comments = Comment.limit(10).order("updated_at desc").all.select{|x| can? :read, x}
     end
   end
 
